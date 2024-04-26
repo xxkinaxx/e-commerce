8a596a07a4139f20b4c919c0f8961216874c5d73
xxkinaxx <ekopandu088@gmail.com>
2024-04-26 09:04:26 +0700

    changes

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -1,1 +1,22 @@
- Ini halaman admin
+@extends('layouts.parent')
+@section('title', 'Dashboard - Admin')
+@section('content')
+<div class="row">
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title">Welcome {{ Auth::user()->name }}</h1>
+
+            <div class="pagetitle">
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item">Components</li>
+                        <li class="breadcrumb-item active">Breadcrumbs</li>
+                    </ol>
+                </nav>
+            </div><!-- End Breadcrumbs with a page title -->
+
+        </div>
+    </div>
+</div>
+@endsection
